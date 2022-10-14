33304bdee6f4d8e684d916b5af6b553decdd14e8
luchenyang <luchenyang8641@gmail.com>
2022-10-14 17:58:54 +0800

    fix

--- src/main/java/com/lcy/demo/springcloud/openfegin/TestApi.java
+++ src/main/java/com/lcy/demo/springcloud/openfegin/TestApi.java
@@ -1,7 +1,6 @@
 package com.lcy.demo.springcloud.openfegin;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @description:
